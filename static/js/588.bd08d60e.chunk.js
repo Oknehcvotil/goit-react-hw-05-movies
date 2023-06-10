188a0b3db2cd9f52942d3d0f3762b62c0797d39e
Oknehcvotil <121458409+Oknehcvotil@users.noreply.github.com>
2023-06-10 15:20:27 +0000

    Deploying to gh-pages from @ Oknehcvotil/goit-react-hw-05-movies@bcb7db2d41b91b84841b23236a34ea9a3c6b5327 🚀

---
+++ static/js/588.bd08d60e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[588],{1588:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});var n=r(1226),u=r(184),c=function(){return(0,u.jsxs)(u.Fragment,{children:[(0,u.jsx)("div",{children:"movie"}),(0,u.jsx)(n.Outlet,{})]})}}}]);
+//# sourceMappingURL=588.bd08d60e.chunk.js.map
