b8ec0ec7a7bcd111dcf53b387d58d4e66735f5a4
Kirill <litovchenko.br@gmail.com>
2023-06-12 02:43:48 +0300

    hw
    removed lazy

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
-import { lazy } from 'react';
+// import { lazy } from 'react';
 import Layout from 'components/Layout';
+import Home from 'pages/Home';
+import Movies from 'pages/Movies';
+import MovieDetails from 'pages/MovieDetails';
+import Cast from 'components/Cast';
+import Reviews from 'components/Reviews';
 
-const Home = lazy(() => import('pages/Home'));
-const Movies = lazy(() => import('pages/Movies'));
-const MovieDetails = lazy(() => import('pages/MovieDetails'));
-const Cast = lazy(() => import('components/Cast'));
-const Reviews = lazy(() => import('components/Reviews'));
+// const Home = lazy(() => import('pages/Home'));
+// const Movies = lazy(() => import('pages/Movies'));
+// const MovieDetails = lazy(() => import('pages/MovieDetails'));
+// const Cast = lazy(() => import('components/Cast'));
+// const Reviews = lazy(() => import('components/Reviews'));
 
 const App = () => {
   return (
